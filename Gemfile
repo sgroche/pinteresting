7039365341beaabdffa520cc67e0c610c2be3b3c
Stephan Roche <sgroche7@gmail.com>
2016-04-19 12:57:27 -0500

    Amazon web services to Heroku

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'devise', '~> 3.5.6'
 gem 'paperclip', '~> 4.2'
+gem 'aws-sdk', '< 2.0'
 gem 'bootstrap-sass'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -19,6 +20,17 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
+
+
+# Sets Paperclip to upload images to Amazon Web Services
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
