5dd2d74a33aac6c6081e06c4827eb52a06c21fdd
Stephan Roche <sgroche7@gmail.com>
2016-04-19 11:56:18 -0500

    User Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
          validates :password, :presence => true,
                      :on => :create,
                      :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
+
+   has_many :pins; 
 end
